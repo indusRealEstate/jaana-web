263e50ef44a62779d05f2ee2f14b835cb8347a70
ashiqjak <ashiqflip3@gmail.com>
2023-10-10 17:57:00 +0400

    10/10/2023 last commit

--- src/components/common/mobile-menu/Social.js
+++ src/components/common/mobile-menu/Social.js
@@ -1,36 +1,36 @@
 const Social = () => {
-  const socialLinks = [
-    {
-      id: 1,
-      iconClass: "fab fa-facebook-f",
-      href: "#",
-    },
-    {
-      id: 2,
-      iconClass: "fab fa-twitter",
-      href: "#",
-    },
-    {
-      id: 3,
-      iconClass: "fab fa-instagram",
-      href: "#",
-    },
-    {
-      id: 4,
-      iconClass: "fab fa-linkedin-in",
-      href: "#",
-    },
-  ];
+	const socialLinks = [
+		{
+			id: 1,
+			iconClass: "fab fa-facebook-f",
+			href: "#",
+		},
+		{
+			id: 2,
+			iconClass: "fab fa-twitter",
+			href: "#",
+		},
+		{
+			id: 3,
+			iconClass: "fab fa-instagram",
+			href: "#",
+		},
+		{
+			id: 4,
+			iconClass: "fab fa-linkedin-in",
+			href: "#",
+		},
+	]
 
-  return (
-    <>
-      {socialLinks.map((link) => (
-        <a className="me-3" href={link.href} key={link.id}>
-          <i className={link.iconClass}></i>
-        </a>
-      ))}
-    </>
-  );
-};
+	return (
+		<>
+			{socialLinks.map((link, index) => (
+				<a className='me-3' href={link.href} key={index}>
+					<i className={link.iconClass}></i>
+				</a>
+			))}
+		</>
+	)
+}
 
-export default Social;
+export default Social
