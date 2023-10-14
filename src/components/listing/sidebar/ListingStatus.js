ee3fdff7cfa726829e27fc188ee10b5dcfbf759e
indusRealEstate <webtech@indusre.ae>
2023-10-14 16:11:07 +0400

    fix: :bug: fixed bugs of useeffect in build stage.

--- src/components/listing/sidebar/ListingStatus.js
+++ src/components/listing/sidebar/ListingStatus.js
@@ -1,46 +1,47 @@
-"use client"
+"use client";
 
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
 const ListingStatus = ({ filterFunctions }) => {
-	const options = [
-		{ id: "flexRadioDefault3", label: "All", defaultChecked: true },
-		{ id: "flexRadioDefault1", label: "Buy" },
-		{ id: "flexRadioDefault2", label: "Rent" },
-	]
+  const options = [
+    { id: "flexRadioDefault3", label: "All", defaultChecked: true },
+    { id: "flexRadioDefault1", label: "Buy" },
+    { id: "flexRadioDefault2", label: "Rent" },
+  ];
 
-	if (filterFunctions?.searchData != undefined) {
-		useEffect(() => {
-			filterFunctions?.handlelistingStatus(filterFunctions?.searchData.tab)
-		}, [filterFunctions?.searchData])
-	}
+  useEffect(() => {
+    if (filterFunctions?.searchData != undefined) {
+      filterFunctions?.handlelistingStatus(filterFunctions?.searchData.tab);
+    }
+  }, [filterFunctions?.searchData]);
 
-	return (
-		<>
-			{options.map((option) => (
-				<div
-					className='form-check d-flex align-items-center mb10'
-					key={option.id}>
-					<input
-						className='form-check-input'
-						type='radio'
-						id={option.id}
-						checked={
-							filterFunctions?.listingStatus.toLowerCase() ==
-							option.label.toLowerCase()
-						}
-						onChange={() => {
-							console.log(option.label.toLowerCase())
-							filterFunctions.handlelistingStatus(option.label)
-						}}
-					/>
-					<label className='form-check-label' htmlFor={option.id}>
-						{option.label}
-					</label>
-				</div>
-			))}
-		</>
-	)
-}
+  return (
+    <>
+      {options.map((option) => (
+        <div
+          className="form-check d-flex align-items-center mb10"
+          key={option.id}
+        >
+          <input
+            className="form-check-input"
+            type="radio"
+            id={option.id}
+            checked={
+              filterFunctions?.listingStatus.toLowerCase() ==
+              option.label.toLowerCase()
+            }
+            onChange={() => {
+              console.log(option.label.toLowerCase());
+              filterFunctions.handlelistingStatus(option.label);
+            }}
+          />
+          <label className="form-check-label" htmlFor={option.id}>
+            {option.label}
+          </label>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default ListingStatus
+export default ListingStatus;
--- src/components/listing/sidebar/SearchBox.js
+++ src/components/listing/sidebar/SearchBox.js
@@ -1,32 +1,32 @@
-"use client"
+"use client";
 
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
 const SearchBox = ({ filterFunctions }) => {
-	if (filterFunctions?.searchData != undefined) {
-		useEffect(() => {
-			filterFunctions?.setSearchQuery(filterFunctions?.searchData.searchText)
-		}, [filterFunctions?.searchData])
-	}
+  useEffect(() => {
+    if (filterFunctions?.searchData != undefined) {
+      filterFunctions?.setSearchQuery(filterFunctions?.searchData.searchText);
+    }
+  }, [filterFunctions?.searchData]);
 
-	return (
-		<div className='search_area'>
-			<input
-				type='text'
-				className='form-control'
-				placeholder='What are you looking for?'
-				defaultValue={
-					filterFunctions?.searchData != undefined
-						? filterFunctions?.searchData.searchText
-						: ""
-				}
-				onChange={(e) => filterFunctions?.setSearchQuery(e.target.value)}
-			/>
-			<label>
-				<span className='flaticon-search' />
-			</label>
-		</div>
-	)
-}
+  return (
+    <div className="search_area">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="What are you looking for?"
+        defaultValue={
+          filterFunctions?.searchData != undefined
+            ? filterFunctions?.searchData.searchText
+            : ""
+        }
+        onChange={(e) => filterFunctions?.setSearchQuery(e.target.value)}
+      />
+      <label>
+        <span className="flaticon-search" />
+      </label>
+    </div>
+  );
+};
 
-export default SearchBox
+export default SearchBox;
