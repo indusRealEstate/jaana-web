5ee9111b9263c8d47bf966cd25a98e36d46c9451
ashiqjak <ashiqflip3@gmail.com>
2023-10-14 15:02:14 +0400

    refactor: :fire: refactor filter system

--- src/components/home/home-v6/hero/HeroContent.js
+++ src/components/home/home-v6/hero/HeroContent.js
@@ -1,87 +1,98 @@
-"use client";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+"use client"
+import React, { useState } from "react"
+import { useRouter } from "next/navigation"
 
-const HeroContent = () => {
-  const router = useRouter();
-  const [activeTab, setActiveTab] = useState("buy");
+const HeroContent = ({ transferData }) => {
+	const router = useRouter()
+	const [activeTab, setActiveTab] = useState("all")
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
+	const tabs = [
+		{ id: "all", label: "All" },
+		{ id: "buy", label: "Buy" },
+		{ id: "rent", label: "Rent" },
+	]
 
-  const tabs = [
-    { id: "buy", label: "Buy" },
-    { id: "rent", label: "Rent" },
-    { id: "sold", label: "Sold" },
-  ];
+	const handleTabClick = (tab) => {
+		setActiveTab(tab.id)
+		transferData?.setSearchProperties({
+			...transferData.searchProperties,
+			tab: tab.label,
+		})
+	}
 
-  return (
-    <div className="advance-search-tab mt60 mt30-md mb25 animate-up-3">
-      <ul className="nav nav-tabs p-0 m-0">
-        {tabs.map((tab) => (
-          <li className="nav-item" key={tab.id}>
-            <button
-              className={`nav-link ${activeTab === tab.id ? "active" : ""}`}
-              onClick={() => handleTabClick(tab.id)}
-            >
-              {tab.label}
-            </button>
-          </li>
-        ))}
-      </ul>
+	return (
+		<div className='advance-search-tab mt60 mt30-md mb25 animate-up-3'>
+			<ul className='nav nav-tabs p-0 m-0'>
+				{tabs.map((tab) => (
+					<li className='nav-item' key={tab.id}>
+						<button
+							className={`nav-link ${activeTab === tab.id ? "active" : ""}`}
+							onClick={() => handleTabClick(tab)}>
+							{tab.label}
+						</button>
+					</li>
+				))}
+			</ul>
 
-      <div className="tab-content">
-        {tabs.map((tab) => (
-          <div
-            className={`${activeTab === tab.id ? "active" : ""} tab-pane`}
-            key={tab.id}
-          >
-            <div className="advance-content-style1">
-              <div className="row">
-                <div className="col-md-8 col-lg-9">
-                  <div className="advance-search-field position-relative text-start">
-                    <form className="form-search position-relative">
-                      <div className="box-search">
-                        <span className="icon flaticon-home-1" />
-                        <input
-                          className="form-control bgc-f7 bdrs12"
-                          type="text"
-                          name="search"
-                          placeholder={`Enter an address, neighborhood, city, or ZIP code for ${tab.label}`}
-                        />
-                      </div>
-                    </form>
-                  </div>
-                </div>
-                {/* End .col-md-8 */}
+			<div className='tab-content'>
+				{tabs.map((tab) => (
+					<div
+						className={`${activeTab === tab.id ? "active" : ""} tab-pane`}
+						key={tab.id}>
+						<div className='advance-content-style1'>
+							<div className='row'>
+								<div className='col-md-8 col-lg-9'>
+									<div className='advance-search-field position-relative text-start'>
+										<form className='form-search position-relative'>
+											<div className='box-search'>
+												<span className='icon flaticon-home-1' />
+												<input
+													className='form-control bgc-f7 bdrs12'
+													type='text'
+													name='search'
+													placeholder={`Search Products for ${tab.label}`}
+													onChange={(event) =>
+														transferData?.setSearchProperties({
+															...transferData.searchProperties,
+															searchText: event.target.value,
+														})
+													}
+												/>
+											</div>
+										</form>
+									</div>
+								</div>
+								{/* End .col-md-8 */}
 
-                <div className="col-md-4 col-lg-3">
-                  <div className="d-flex align-items-center justify-content-start justify-content-md-center mt-3 mt-md-0">
-                    <button
-                      className="advance-search-btn"
-                      type="button"
-                      data-bs-toggle="modal"
-                      data-bs-target="#advanceSeachModal"
-                    >
-                      <span className="flaticon-settings" /> Advanced
-                    </button>
-                    <button
-                      className="advance-search-icon ud-btn btn-thm ms-4"
-                      type="button"
-                      onClick={() => router.push("/grid-full-2-col")}
-                    >
-                      <span className="flaticon-search" />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+								<div className='col-md-4 col-lg-3'>
+									<div className='d-flex align-items-center justify-content-start justify-content-md-center mt-3 mt-md-0'>
+										<button
+											className='advance-search-btn'
+											type='button'
+											data-bs-toggle='modal'
+											data-bs-target='#advanceSeachModal'>
+											<span className='flaticon-settings' /> Advanced
+										</button>
+										<button
+											className='advance-search-icon ud-btn btn-thm ms-4'
+											type='button'
+											onClick={(event) => {
+												event.preventDefault()
+												if (event.type === "click") {
+													transferData?.search(transferData?.searchProperties)
+												}
+											}}>
+											<span className='flaticon-search' />
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
 
-export default HeroContent;
+export default HeroContent
