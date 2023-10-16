a01048d05e43056e5ede8fe23c4aeeaf80705839
indusRealEstate <webtech@indusre.ae>
2023-10-16 15:18:57 +0400

    refactor: :zap: refactored the agent details page.

---
+++ src/data/agent-details.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "Jaana Markkanen",
+  mobile_no: "+971 505527479",
+  email: "jaana.markkanen@indusre.ae",
+  description:
+    "With over years of experience in the dynamic world of real estate, I've successfully helped countless clients achieve their homeownership dreams. Whether you're a first-time buyer, seasoned investor, or looking to sell your property, my extensive knowledge of the market and unwavering dedication ensure a smooth and profitable real estate journey for you. With over years of experience in the dynamic world of real estate, I've successfully helped countless clients achieve their homeownership dreams. Whether you're a first-time buyer, seasoned investor, or looking to sell your property, my extensive knowledge of the market and unwavering dedication ensure a smooth and profitable real estate journey for you.",
+};
