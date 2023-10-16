1fc12eaafb45e18126d3cba53d9e30ab7299898f
indusRealEstate <webtech@indusre.ae>
2023-10-16 11:05:46 +0400

    fix: :bug: fixed few bugs.

--- src/components/home/FilterProperties.js
+++ src/components/home/FilterProperties.js
@@ -142,7 +142,7 @@ const FilterProperties = () => {
                   }}
                 >
                   <Image
-                    src={`/images/svg/images/No data-pana.svg`}
+                    src={`/images/svg/no-data.svg`}
                     width={300}
                     height={300}
                     alt="No data Image"
