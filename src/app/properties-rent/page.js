1a29e396369816d3217362a18559e23f62093f64
indusRealEstate <webtech@indusre.ae>
2023-10-18 15:10:37 +0400

    refactor: :sparkles: seperated client pages from server for meta tags.

--- src/app/properties-rent/page.js
+++ src/app/properties-rent/page.js
@@ -1,88 +1,11 @@
-"use client"
-import DefaultHeader from "@/components/common/DefaultHeader"
+import PropertiesRent from ".";
 
-import Footer from "@/components/common/default-footer"
-import MobileMenu from "@/components/common/mobile-menu"
+export const metadata = {
+  title: "Top Realtor Dubai | Explore Properties",
+};
 
-import ProperteyFiltering from "@/components/listing/grid-view/properties-rent/PropertyFiltering"
-import { useEffect, useState } from "react"
-import { getAllRentProperties } from "@/api/properties"
-import { Box, CircularProgress } from "@mui/material"
+const PropertiesRentPage = () => {
+  return <PropertiesRent />;
+};
 
-import React from "react"
-
-const PropertiesRent = () => {
-	const [rentProperties, setRentProperties] = useState([])
-
-	getAllRentProperties().then((response) => {
-		setRentProperties(response.prop)
-	})
-	//console.log(rentProperties);
-	return (
-		<>
-			{/* Main Header Nav */}
-			<DefaultHeader />
-			{/* End Main Header Nav */}
-
-			{/* Mobile Nav  */}
-			<MobileMenu />
-			{/* End Mobile Nav  */}
-
-			{/* Breadcumb Sections */}
-			<section className='breadcumb-section bgc-f7'>
-				<div className='container'>
-					<div className='row'>
-						<div className='col-lg-12'>
-							<div className='breadcumb-style1'>
-								<h2 className='title'>Properties For Rent</h2>
-								<div className='breadcumb-list'>
-									<a href='/'>Home</a>
-									<a href='#'>For Rent</a>
-								</div>
-								<a
-									className='filter-btn-left mobile-filter-btn d-block d-lg-none'
-									data-bs-toggle='offcanvas'
-									href='#listingSidebarFilter'
-									role='button'
-									aria-controls='listingSidebarFilter'>
-									<span className='flaticon-settings' /> Filter
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			{/* End Breadcumb Sections */}
-
-			{/* Property Filtering */}
-
-			{/* <ProperteyFiltering listings = {rentProperties}/> */}
-			{rentProperties == "" || rentProperties == undefined ? (
-				<>
-					<Box
-						sx={{
-							display: "flex",
-							height: "50vh",
-							width: "100%",
-							justifyContent: "center",
-							alignItems: "center",
-						}}>
-						<CircularProgress size={50}></CircularProgress>
-					</Box>
-				</>
-			) : (
-				<ProperteyFiltering listings={rentProperties} />
-			)}
-
-			{/* Property Filtering */}
-
-			{/* Start Our Footer */}
-			<section className='footer-style1 pt60 pb-0'>
-				<Footer />
-			</section>
-			{/* End Our Footer */}
-		</>
-	)
-}
-
-export default PropertiesRent
+export default PropertiesRentPage;
