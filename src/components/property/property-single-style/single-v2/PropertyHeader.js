61f06b5c5c019c98ca7247438526882d7d1fe328
ashiqjak <ashiqflip3@gmail.com>
2023-10-18 14:27:41 +0400

    feat:

--- src/components/property/property-single-style/single-v2/PropertyHeader.js
+++ src/components/property/property-single-style/single-v2/PropertyHeader.js
@@ -1,16 +1,100 @@
-import Box from "@mui/material/Box"
-import Skeleton from "@mui/material/Skeleton"
+import {
+	Box,
+	Button,
+	Tooltip,
+	Skeleton,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Slide,
+} from "@mui/material"
+import { useState } from "react"
+import { TransitionProps } from "@mui/material/transitions"
+import Image from "next/image"
+import Link from "next/link"
 
 const PropertyHeader = ({ headerdata }) => {
 	// const data = listings.filter((elm) => elm.id == id)[0] || listings[0];
+	const [share, setShare] = useState(false)
+	const [open, setOpen] = useState(false)
 	const formatter = new Intl.NumberFormat("en-US", {
 		style: "currency",
 		currency: "AED",
 	})
 
-	// console.log
+	const printPage = (event) => {
+		if (event.type === "click") {
+			window.print()
+		}
+	}
+	const openNewWindow = (event) => {
+		// window.open(window.location.href)
+		window.open(
+			document.URL,
+			"_blank",
+			"location=yes,height=720,width=1200,scrollbars=yes,status=yes",
+			window.location.href,
+		)
+	}
+
+	const openSharBtn = (event) => {
+		if (event.type == "click") {
+			setShare(true)
+			handleClickOpen()
+		}
+	}
+
+	const handleClickOpen = () => {
+		setOpen(true)
+	}
+
+	const handleClose = () => {
+		setOpen(false)
+	}
 	return (
 		<>
+			{share ? (
+				<Dialog
+					open={open}
+					// TransitionComponent={Transition}
+					keepMounted
+					onClose={handleClose}
+					aria-describedby='alert-dialog-slide-description'>
+					<DialogTitle>
+						<div className='row'>
+							<div className='col-3'>
+								<Link
+									style={{
+										padding: 0,
+										border: "none",
+										background: "none",
+									}}
+									href={`whatsapp://send?text=${window.location.href}" data-action="share/whatsapp/share" target="_blank"`}>
+									<Image
+										width={50}
+										height={50}
+										src='/images/socialMedia/whatsapp.svg'
+									/>
+								</Link>
+							</div>
+						</div>
+					</DialogTitle>
+					<DialogContent>
+						<DialogContentText id='alert-dialog-slide-description'>
+							Let Google help apps determine location. This means sending
+							anonymous location data to Google, even when no apps are running.
+						</DialogContentText>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose}>Disagree</Button>
+						<Button onClick={handleClose}>Agree</Button>
+					</DialogActions>
+				</Dialog>
+			) : (
+				<></>
+			)}
 			<div className='col-lg-8'>
 				<div className='single-property-content mb30-md'>
 					<h2 className='sp-lg-title green-title-theme'>
@@ -66,18 +150,48 @@ const PropertyHeader = ({ headerdata }) => {
 				<div className='single-property-content'>
 					<div className='property-action text-lg-end'>
 						<div className='d-flex mb20 mb10-md align-items-center justify-content-lg-end'>
-							<a className='icon mr10' href='#'>
+							{/* <a className='icon mr10' href='#'>
 								<span className='flaticon-like' />
-							</a>
-							<a className='icon mr10' href='#'>
-								<span className='flaticon-new-tab' />
-							</a>
-							<a className='icon mr10' href='#'>
-								<span className='flaticon-share-1' />
-							</a>
-							<a className='icon'>
-								<span className='flaticon-printer' />
-							</a>
+							</a> */}
+							<Tooltip title='New Window' placement='top'>
+								<button
+									onClick={(event) => openNewWindow(event)}
+									className='icon mr10'
+									style={{
+										background: "none",
+										padding: "0",
+										// border: "none",
+									}}
+									href='#'>
+									<span className='flaticon-new-tab' />
+								</button>
+							</Tooltip>
+
+							<Tooltip title='Share' placement='top'>
+								<button
+									onClick={(event) => openSharBtn(event)}
+									style={{
+										background: "none",
+										padding: "0",
+										// border: "none",
+									}}
+									className='icon mr10'
+									href='#'>
+									<span className='flaticon-share-1' />
+								</button>
+							</Tooltip>
+							<Tooltip title='Print' placement='top'>
+								<button
+									style={{
+										background: "none",
+										padding: "0",
+										// border: "none",
+									}}
+									onClick={(event) => printPage(event)}
+									className='icon'>
+									<span className='flaticon-printer' />
+								</button>
+							</Tooltip>
 						</div>
 						<h3 className='price mb-0 dark-title-theme'>
 							{formatter.format(headerdata.price).replace(".00", "")}
